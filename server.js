abc99069b74f0ddc6f525b8e5449d13876c9d3e2
Vincent Kipkoech <vincent.kipkoech2@student.moringaschool.com>
2026-04-24 01:19:56 +0300

    changes in middleware

--- server.js
+++ server.js
@@ -11,7 +11,12 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: ["http://127.0.0.1:5500", "http://localhost:5500"], // Allows your Live Server
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
+}));
 app.use(express.json()); 
 app.use(express.static('public')); 
 
