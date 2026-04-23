116a8890c2c41208c20dcb513b716b6b74a42e71
Vincent Kipkoech <vincent.kipkoech2@student.moringaschool.com>
2026-04-24 01:27:44 +0300

    changes in server.js

--- server.js
+++ server.js
@@ -17,6 +17,17 @@ app.use(cors({
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
 }));
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    
+    // Crucial: Handle the browser's "Preflight" check
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+});
 app.use(express.json()); 
 app.use(express.static('public')); 
 
